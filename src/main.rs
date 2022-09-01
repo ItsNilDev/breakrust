69f1210613fa49d7a471469d2a77ecd3df4ae4f7
ItsNil <poria2100@gmail.com>
2022-09-01 13:29:25 +0430

    Refactor & Player Collisions

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
 use macroquad::prelude::{*, scene::clear};
-use simplelog::*;
+use simplelog::{TermLogger, LevelFilter, Config, TerminalMode, ColorChoice};
 
 struct Player {
 	rec_width: f32,
 	rec_height: f32,
-	width: f32,
+	x: f32,
 }
 
 struct Ball {
-	width: f32,
-	height: f32,
+	x: f32,
+	y: f32,
 	dx: f32,
 	dy: f32,
 	launched: bool,
 }
 
 fn conf() -> Conf {
-    Conf {
-        window_title: String::from("Macroquad"),
-        window_width: 800,
-        window_height: 800,
-        fullscreen: false,
-        ..Default::default()
-    }
+	Conf {
+		window_title: String::from("Macroquad"),
+		window_width: 800,
+		window_height: 800,
+		fullscreen: false,
+		..Default::default()
+	}
 }
 
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
 	let mut player = Player {
-		rec_width: 130.0,
-		rec_height: 20.0,
-		width: screen_width() / 2.0 - 60.0,
+		rec_width: 130.,
+		rec_height: 20.,
+		x: screen_width() / 2.0 - 60.,
 	};
 	let mut game_over = false;
 	let mut ball = Ball {
-		width: player.width + 65.0,
-		height: screen_height() - 65.0,
-		dx: 300.0,
-		dy: -300.0,
+		x: player.x + 65.0,
+		y: screen_height() - 65.0,
+		dx: 300.,
+		dy: -300.,
 		launched: false,
 	};
 	TermLogger::new(
@@ -49,7 +49,7 @@ async fn main() {
 
 	// fn launch_ball(ball: &mut Ball, movement_speed: f32) {
 	// 	let delta = get_frame_time();
-	// 	if (*ball).width > screen_width() - 20.0 {
+	// 	if (*ball).width > screen_width() - 20. {
 	// 		(*ball).width
 	// 	}
 	// 	(*ball).width  += delta * movement_speed;
@@ -62,21 +62,21 @@ async fn main() {
 		if !game_over {
 			if is_key_down(KeyCode::I) {
 				// 30 is for the max width that player can go
-				if player.width <= screen_width() - player.rec_width - 30.0 {
-					player.width += 10.0;
+				if player.x <= screen_width() - player.rec_width - 30. {
+					player.x += 10.;
 				}
 				if !ball.launched {
-					ball.width = player.width + 65.0;
+					ball.x = player.x + 65.0;
 				}
 			}
 
 			if is_key_down(KeyCode::N) {
 				// 30 is min here
-				if player.width > 30.0 {
-					player.width -= 10.0;
+				if player.x > 30. {
+					player.x -= 10.;
 				}
 				if !ball.launched {
-					ball.width = player.width + 65.0;
+					ball.x = player.x + 65.0;
 				}
 
 			}
@@ -86,33 +86,57 @@ async fn main() {
 			}
 
 			if ball.launched {
-				// launch_ball(&mut ball, 100.0);
 				let delta = get_frame_time();
-				if ball.width > screen_width() || ball.width < 0.0 {
+
+				// right collision
+				if ball.x >= screen_width() {
 					ball.dx = -ball.dx;
+					ball.x  = screen_width();
 				}
-				if ball.height < 0.0{
+
+				// left collision
+				if ball.x <= 0. {
+					ball.dx = -ball.dx;
+					ball.x  = 0.;
+				}
+
+				// up collision
+				if ball.y < 0.{
 					ball.dy = -ball.dy;
+					ball.y = 0.;
 				}
-				if ball.height > screen_height() {
+
+				// down collision
+				if ball.y > screen_height() {
+					// can we set these things to default by any chance?
 					game_over = true;
-					player.width = screen_width() / 2.0 - 60.0;
-					ball.width = player.width + 65.0;
-					ball.height = screen_height() - 65.0;
-					ball.dx = 300.0;
-					ball.dy = -300.0;
+					player.x = screen_width() / 2.0 - 60.;
+					ball.x = player.x + 65.0;
+					ball.y = screen_height() - 65.0;
+					ball.dx = 300.;
+					ball.dy = -300.;
 					ball.launched = false;
 				}
-				ball.width  += delta * ball.dx;
-				ball.height += delta * ball.dy;
+
+				// player-ball collision
+				// height detection:
+				if ball.y >= screen_height() - player.rec_height - 40.
+				// width detection:
+					&& ball.x <= player.x + player.rec_width
+					&& ball.x >= player.x {
+						// FIXME: some bugs
+						ball.dy = -ball.dy;
+					}
+				ball.x  += delta * ball.dx;
+				ball.y += delta * ball.dy;
 			}
 		}
 
 		if !game_over {
-			clear_background(DARKGRAY);
-			draw_rectangle(player.width, screen_height() - 50.0, player.rec_width, player.rec_height, GREEN);
+			clear_background(BLACK);
+			draw_rectangle(player.x, screen_height() - 50., player.rec_width, player.rec_height, RED);
 
-			draw_circle(ball.width, ball.height, 15.0, GREEN);
+			draw_circle(ball.x, ball.y, 15.0, WHITE);
 		} else {
 			clear_background(RED);
 			if is_key_pressed(KeyCode::Enter) {
@@ -122,7 +146,6 @@ async fn main() {
 				game_over = false;
 			}
 		}
-		debug!("r");
 		next_frame().await
 	}
 }
